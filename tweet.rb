ba0bc845e54cbee4925b959ccabc1fe2dbe6936a
naotsukamoto <m0naaa0u@gmail.com>
2020-02-04 02:33:09 +0900

    japanese changed

--- tweet.rb
+++ tweet.rb
@@ -9,7 +9,7 @@ class Tweet
     # 投稿内容の初期化
     @text = ""
     # 投稿内容(タグ)
-    @text_tag = " #国名わけっこ #ジャルジャル #英語ver"
+    @text_tag = "#国名わけっこゲーム #ジャルジャル #M1 #日本語ver"
     # クライアントの生成
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['CONSUMER_KEY']
@@ -17,11 +17,18 @@ class Tweet
       config.access_token        = ENV['ACCESS_TOKEN']
       config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
     end
+    ## 日本語変換
+    ISO3166.configure do |config|
+      config.locales = [:en, :de, :fr, :es, :ja]
+    end
     # 国名取得
     @c = ISO3166::Country.all
-    @c_name = []
-    @c.each do |country|
-        @c_name << country.name
+    @c_one = @c[rand(@c.length)]
+    @c_name = @c_one.translations['ja']
+    # 文字数が短いときは国名を変更  
+    while @c_name.length < 4
+      @c_one = @c[rand(@c.length)]
+      @c_name = @c_one.translations['ja']
     end
   end
 
@@ -30,7 +37,7 @@ class Tweet
     creat_content
     create_text
     update
-    sleep(300)
+    # sleep(300)
     create_text2
     update2
   end
@@ -41,10 +48,8 @@ class Tweet
     my_user = @client.user
     @tc = my_user.tweets_count
     @tc +=1
-    # 国名をランダムで取得
-    @c_name1 = @c_name[rand(@c_name.length)]
     # 国名を任意のところで区切る
-    c_name_split = @c_name1.split(/\A(.{1,#{rand(@c_name1.length)}})/,2)[1..-1]
+    c_name_split = @c_name.split(/\A(.{2,#{rand(@c_name.length)}})/,2)[1..-1]
     # 前と後の文字を格納
     @c_name_split_first = c_name_split[0]
     @c_name_split_last = c_name_split[1]
@@ -75,7 +80,7 @@ class Tweet
 
   def create_text2
     # 格納
-    @text2 = "##{@tc.to_s} Ans.\n#{@c_name_split_first}\nFULL:#{@c_name1}\n#{@text_tag}"
+    @text2 = "##{@tc.to_s} Ans.\n#{@c_name_split_last}\nFull:#{@c_name} #{@c_one.emoji_flag}\n#{@text_tag}"
   end
 
 
