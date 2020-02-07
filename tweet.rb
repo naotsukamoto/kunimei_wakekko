5d093ace5d8d53d8697be7281f60caf6ff3ca917
naotsukamoto <m0naaa0u@gmail.com>
2020-02-08 01:19:43 +0900

    three countries changed from one to three

--- tweet.rb
+++ tweet.rb
@@ -9,7 +9,7 @@ class Tweet
     # 投稿内容の初期化
     @text = ""
     # 投稿内容(タグ)
-    @text_tag = "#国名わけっこゲーム #ジャルジャル #M1 #日本語ver"
+    # @text_tag = "#国名わけっこゲーム #ジャルジャル #M1 #日本語ver"
     # クライアントの生成
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['CONSUMER_KEY']
@@ -23,13 +23,31 @@ class Tweet
     end
     # 国名取得
     @c = ISO3166::Country.all
-    @c_one = @c[rand(@c.length)]
-    @c_name = @c_one.translations['ja']
-    # 文字数が短いときは国名を変更  
-    while @c_name.length < 4
-      @c_one = @c[rand(@c.length)]
-      @c_name = @c_one.translations['ja']
+    # @c_one = @c[rand(@c.length)]
+    # 複数の国名取得and日本語に変更
+    @c_select =  []
+    3.times do |n|
+      @c_select << @c[rand(@c.length)]
+      @c_select_name = @c_select.map{|s| s.translations['ja']}
     end
+    # 文字数が短いときは国名を変更  
+    3.times do |x|
+      while @c_select_name[x].length < 4
+        @c_select[x] = @c[rand(@c.length)]
+        @c_select_name[x] = @c_select[x].translations['ja']
+      end
+    end
+    
+    # @c_name = @c_one.translations['ja']
+    # # 文字数が短いときは国名を変更  
+    # while @c_name.length < 4
+    #   @c_one = @c[rand(@c.length)]
+    #   @c_name = @c_one.translations['ja']
+    # end
+
+    # 投稿内容(タグ)
+    @text_tag1 = "#国名わけっこゲーム #ジャルジャル #M1"
+    @text_tag2 = "#国名わけっこゲーム #ジャルジャル #M1 ##{@c_select_name[0]} ##{@c_select_name[1]} ##{@c_select_name[2]}"
   end
 
   # Tweetの投稿処理呼び出し
@@ -37,7 +55,8 @@ class Tweet
     creat_content
     create_text
     update
-    # sleep(300)
+    # 10分後に答えをツイートする
+    sleep(600)
     create_text2
     update2
   end
@@ -47,21 +66,23 @@ class Tweet
     # 投稿内容を設定
     my_user = @client.user
     @tc = my_user.tweets_count
-    @tc +=1
-    # 国名を任意のところで区切る
-    c_name_split = @c_name.split(/\A(.{2,#{rand(@c_name.length)}})/,2)[1..-1]
-    # 前と後の文字を格納
-    @c_name_split_first = c_name_split[0]
-    @c_name_split_last = c_name_split[1]
+    @tc = @tc/2 + 1
+    # 3つの国名を任意のところで区切る
+    min = @c_select_name.map(&:length).min - 2
+    @c_select_name_split = []
+    @c_select_name.each do |s|
+      @c_select_name_split << s.split(/\A(.{2,#{min}})/,2)[1..-1]
+    end
+    # @c_select_name_split = @c_select_name.map{|n| n.split(/\A(.{2,#{min}})/,2)[1..-1]}
+    # # 国名を任意のところで区切る
+    # c_name_split = @c_name.split(/\A(.{2,#{rand(@c_name.length)}})/,2)[1..-1]
+    # # 前と後の文字を格納
+    # @c_name_split_first = c_name_split[0]
+    # @c_name_split_last = c_name_split[1]
   end
 
   # ツイート本文の生成
   def create_text
-    # # 投稿内容を設定
-    # my_user = @client.user
-    # tc = my_user.tweets_count
-    # # 投稿id化
-    # tc += 1
     # # 国名をランダムで取得
     # c_name = @c_name[rand(@c_name.length)]
     # # 国名を任意のところで区切る
@@ -69,18 +90,17 @@ class Tweet
     # # 前と後の文字を格納
     # c_name_split_first = c_name_split[0]
     # c_name_split_last = c_name_split[1]
-    # 格納
-    @text = "##{@tc.to_s} Ques.\n#{@c_name_split_first}\n#{@text_tag}"
-    # if tc % 2 == 0 then
-    #     @text = "これはQuestionです:#{c_name_split_first}:#{tc.to_s}回目です"
-    # else
-    #     @text = "これはQuestionです:#{c_name_last}:#{tc.to_s}回目です"
-    # end
+    # # 格納
+    # @text = "#{@c_name_split_first}\n#{@text_tag}"
+    @text = "##{@tc.to_s} Q.\n#{@c_select_name_split[0][0]}#{@c_select_name_split[1][0]}#{@c_select_name_split[2][0]}\n#{@text_tag1}"
   end
 
   def create_text2
-    # 格納
-    @text2 = "##{@tc.to_s} Ans.\n#{@c_name_split_last}\nFull:#{@c_name} #{@c_one.emoji_flag}\n#{@text_tag}"
+    # # 格納
+    # @tc +=1
+    # @text2 = "#{@c_name_split_last}\nFull:#{@c_name} #{@c_one.emoji_flag}\n#{@text_tag}"
+    @text2 = "##{@tc.to_s} A.\n#{@c_select_name_split[0][1]}#{@c_select_name_split[1][1]}#{@c_select_name_split[2][1]}\n
+    FULL:#{@c_select_name[0]}/#{@c_select_name[1]}/#{@c_select_name[2]}\n#{@text_tag2}"
   end
 
 
